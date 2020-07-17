793a5998652ea595866a63ee418fa6cf70a625d5
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-18 00:21:17 +0500

    global styling file

---
+++ Styles/GlobalStyles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  text: {
+    fontFamily: 'Nunitos-Regular',
+    fontSize: 24,
+    color: '#333',
+    // fontWeight: '700',
+  },
+  para: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
