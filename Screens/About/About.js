058a106e678afd9d4a242a7afc273810bca1434c
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-18 00:20:29 +0500

    About Screen

---
+++ Screens/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {globalStyles} from '../../Styles/GlobalStyles';
+
+const About = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.text}>About Screen</Text>
+    </View>
+  );
+};
+
+export default About;
