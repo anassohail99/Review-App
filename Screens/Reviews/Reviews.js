29e5f3ea747396e0beb58a50df27f32e7b4208e0
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-18 00:21:06 +0500

    Review screen

---
+++ Screens/Reviews/Reviews.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {globalStyles} from '../../Styles/GlobalStyles';
+
+const Reviews = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.text}>Reviews Screen</Text>
+    </View>
+  );
+};
+
+export default Reviews;
