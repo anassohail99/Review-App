9dcf460762eed0da9dfd865674890359a9ca7b0a
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-18 01:05:29 +0500

    Refactoring of color

--- Styles/GlobalStyles.js
+++ Styles/GlobalStyles.js
@@ -4,11 +4,13 @@ export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    // backgroundColor: 'rgba(206, 74, 126, 1)',
+    backgroundColor: 'rgba(28, 28, 27, 1)',
   },
   text: {
     fontFamily: 'Nunitos-Regular',
     fontSize: 24,
-    color: '#333',
+    color: 'rgba(206, 74, 126, 1)',
     // fontWeight: '700',
   },
   para: {
