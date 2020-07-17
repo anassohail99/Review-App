53ab0490151c92f99a4be655681d85b6d8ec8553
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-18 01:04:19 +0500

    Navigations Options added

--- Routes/HomeStack.js
+++ Routes/HomeStack.js
@@ -6,13 +6,27 @@ import Reviews from '../Screens/Reviews/Reviews';
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: 'GameZone',
+      textAlign: 'center',
+    },
   },
   Reviews: {
     screen: Reviews,
+    navigationOptions: {
+      title: 'Review Details',
+    },
   },
 };
 
 // used to create a new stack navigator
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#fff',
+    headerStyle: {
+      backgroundColor: '#b43166',
+    },
+  },
+});
 
 export default createAppContainer(HomeStack);
