458f11cd7e121718bd1e2009583eaf7d750c49bc
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-18 01:04:43 +0500

    Dummy data added and passed as props

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
 import {globalStyles} from '../../Styles/GlobalStyles';
 
 const Home = ({navigation}) => {
@@ -7,10 +7,42 @@ const Home = ({navigation}) => {
     navigation.navigate('Reviews');
   };
 
+  const [review, setReview] = useState([
+    {
+      title: 'Film 1',
+      rating: 5,
+      body: 'lorem ispum',
+      key: '1',
+    },
+    {
+      title: 'Film 2',
+      rating: 4,
+      body: 'lorem ispum',
+      key: '2',
+    },
+    {
+      title: 'Film 3',
+      rating: 2,
+      body: 'lorem ispum',
+      key: '3',
+    },
+  ]);
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.text}>Home Screen</Text>
-      <Button title="Check Reviews" onPress={onPressHandler} />
+      <FlatList
+        data={review}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Reviews', item);
+            }}>
+            <Text style={globalStyles.text}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      {/* <Button title="Check Reviews" onPress={onPressHandler} /> */}
     </View>
   );
 };
